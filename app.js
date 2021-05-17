a4256941d71f59bf2bf56b1c9abf2b95f9bb1007
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2021-05-17 10:11:38 +0300

    link schema, started link route

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/link', require('./routes/link.routes'))
 
 const PORT = config.get('port') || 5000
 
