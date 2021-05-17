3984d788314732f545a893288159876bd034e646
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2021-05-17 13:04:22 +0300

    generating link (router.post /generate)

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,10 +1,32 @@
 const {Router} = require('express')
+const config = require('config')
+const shortid = require('shortid')
 const Link = require('../models/Link')
 const auth = require('../middleware/auth.middleware')
 const router = Router()
 
-router.post('/generate', async (req, res) => {
+router.post('/generate', auth, async (req, res) => {
   try {
+    const baseUrl = config.get('baseUrl')
+    const {from} = req.body //redirect user by this link
+
+    const code = shortid.generate()
+
+    const existing = await Link.findOne({ from })
+
+    if (existing) {
+      return res.json({link: existing})
+    }
+
+    const to = baseUrl + '/t/' + code //shorten link
+
+    const link = new Link ({
+      code, to, from, owner: req.user.userId
+    })
+
+    await link.save()
+
+    res.status(201).json({ link })
 
   } catch (e) {
     res.status(500).json({ message: 'Something went wrong, try again' })
@@ -13,14 +35,14 @@ router.post('/generate', async (req, res) => {
 
 router.get('/', auth, async (req, res) => {
   try {
-    const links = await Link.find({owner: req.user.userId})
+    const links = await Link.find({ owner: req.user.userId })
     res.json(links)
   } catch (e) {
     res.status(500).json({ message: 'Something went wrong, try again' })
   }
 })
 
-router.get('/generate', async (req, res) => {
+router.get('/generate', auth, async (req, res) => {
   try {
     const link = await Link.findById(req.params.id)
     res.json(link)
