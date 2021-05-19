0f9919a7c7f5dae82424cb26abb8dc3615e1c54c
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2021-05-19 22:27:41 +0300

    loader

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,6 +1,9 @@
 import "materialize-css";
+import {useState} from 'react'
 
 export const DetailPage = () => {
+  const [link, setLink] = useState(null)
+
   return (
     <div>
       <h1>DetailPage</h1>
