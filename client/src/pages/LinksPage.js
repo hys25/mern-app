1d74e851a674f8a059feab14b9eed646e1a44993
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2021-05-27 23:22:31 +0300

    LinksPage component

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,34 @@
+import {useState} from 'react'
 import "materialize-css";
+import {useHttp} from "../hooks/http.hook";
+import {Loader} from "../components/Loader";
+import {LinkCard} from "../components/LinkCard";
 
 export const LinksPage = () => {
+  const [links, setLinks] = useState([])
+  const{loading, request} = useHttp()
+  const {token} = useContext(AuthContext)
+
+  const fetchLinks = useCallback(async() => {
+    try {
+      const fetched = await request('/api/link', 'GET', null, {
+        Authorization: `Bearer ${token}`
+      })
+      setLinks(fetched)
+    } catch (e) {}
+  }, [token, request])
+
+  useEffect( () => {
+    fetchLinks()
+  }, [fetchLinks])
+
+  if (loading) {
+    return <Loader />
+  }
+
   return (
-    <div>
-      <h1>LinksPage</h1>
-    </div>
+    <>
+      {!loading && <LinksList links={links} />}
+    </>
   )
 }
